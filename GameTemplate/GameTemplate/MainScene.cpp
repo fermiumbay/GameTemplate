a89064a68c615a500b10e05d5580c48d1bfedcfa
Fermiumbay <fermiumbay131072@gmail.com>
2014-10-03 13:14:49 +0900

    サンプルコード削除

--- GameTemplate/GameTemplate/MainScene.cpp
+++ GameTemplate/GameTemplate/MainScene.cpp
@@ -1,49 +1,22 @@
 #include "MainScene.h"
 
 MainScene::MainScene(){
-	SetFontSize(24);	// 負荷が重いらしいので、頻繁に呼び出さないよう注意
-	ChangeFont("メイリオ");	// 負荷が重いらしいので、頻繁に呼び出さないよう注意
-	text = "キャンセルキーで終了します";
-
-	pin = new Pin();
-	ChangeState(State::b);
+	font["テキスト"] = Font::Create("メイリオ", Vector2d(10, 10), 24);
 }
 
 MainScene::~MainScene(){
-	delete pin;
 }
 
 void MainScene::Update(){
 	BaseScene::Update();
 
-	//if (KeyInput::GetKey(Cancel, PushedNow)){
-	//	EndGame();
-	//}
-
-	pin->Update();
-	if (pin->GetEndFlg()){
-		ChangeScene("Main");
-	}
-	stringstream ss;
-	string i;
-	ss << GetStateTime();
-	ss >> i;
-	switch (GetState()){
-	case State::a:
-		text = "a" + i;
-		break;
-	case State::b:
-		text = "b" + i;
-		break;
-	case State::c:
-		text = "c" + i;
-		break;
+	if (KeyInput::GetKey(Cancel, PushedNow)){
+		EndGame();
 	}
 }
 
 void MainScene::Draw(){
 	BaseScene::Draw();
 
-	pin->Draw();
-	DrawFormatString(10, 10, GetColor(255, 255, 255), text.c_str());
+	font["テキスト"]->Print("キャンセルキーで終了します");
 }
--- GameTemplate/GameTemplate/MainScene.h
+++ GameTemplate/GameTemplate/MainScene.h
@@ -1,16 +1,8 @@
 #pragma once
 #include "BaseScene.h"
-#include "Pin.h"
 
 // ゲーム画面クラス
 class MainScene : public BaseScene{
-	enum State{
-		a,
-		b,
-		c,
-	};
-	string text;	// 表示するテキスト
-	Pin* pin;
 public:
 	MainScene();	// コンストラクタ
 	~MainScene();	// デストラクタ
