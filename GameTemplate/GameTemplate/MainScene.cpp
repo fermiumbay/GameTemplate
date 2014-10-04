2e8140180c8f6d11b848396281573314b859d0fa
Fermiumbay <fermiumbay131072@gmail.com>
2014-10-04 09:18:58 +0900

    【説明書修正】ver1.03へのバージョンアップに伴い、説明書の内容を修正、加筆した。

--- GameTemplate/GameTemplate/MainScene.cpp
+++ GameTemplate/GameTemplate/MainScene.cpp
@@ -1,7 +1,11 @@
 #include "MainScene.h"
+using namespace common;
 
 MainScene::MainScene(){
-	font["テキスト"] = Font::Create("メイリオ", Vector2d(10, 10), 24);
+	font["テキスト"] = Font::Create("メイリオ", Vector2d(10, 10), 32);
+	pic["ぴんくり"] = Graphic::Create("..\\..\\..\\ぴんくりちゃん\\ぴんくりちゃん\\bossface7.png", common::GetWindowCenter(), true);
+	reverseFlg = false;
+	SetKeys();
 }
 
 MainScene::~MainScene(){
@@ -10,13 +14,66 @@ MainScene::~MainScene(){
 void MainScene::Update(){
 	BaseScene::Update();
 
-	if (KeyInput::GetKey(Cancel, PushedNow)){
-		EndGame();
+	if (KeyInput::GetKey(Left, HasBePushed)){
+		pic["ぴんくり"]->pos.x -= 3;
+	}
+	if (KeyInput::GetKey(Right, HasBePushed)){
+		pic["ぴんくり"]->pos.x += 3;
+	}
+	if (KeyInput::GetKey(Up, HasBePushed)){
+		pic["ぴんくり"]->pos.y -= 3;
+	}
+	if (KeyInput::GetKey(Down, HasBePushed)){
+		pic["ぴんくり"]->pos.y += 3;
+	}
+	if (KeyInput::GetKey(OK, PushedNow)){
+		reverseFlg ^= 1;	// 反転
+		SetKeys();
 	}
 }
 
 void MainScene::Draw(){
 	BaseScene::Draw();
 
-	font["テキスト"]->Print("キャンセルキーで終了します");
+	pic["ぴんくり"]->Draw();
+
+	if (!reverseFlg){
+		font["テキスト"]->Print("通常");
+	}
+	else{
+		font["テキスト"]->Print("反転");
+	}
+}
+
+void MainScene::SetKeys(){
+	if (!reverseFlg){
+		// 通常
+		KeyInput::SetKeyCond(Left, []{
+			return CheckHitKey(KEY_INPUT_LEFT) != 0;
+		});
+		KeyInput::SetKeyCond(Right, []{
+			return CheckHitKey(KEY_INPUT_RIGHT) != 0;
+		});
+		KeyInput::SetKeyCond(Up, []{
+			return CheckHitKey(KEY_INPUT_UP) != 0;
+		});
+		KeyInput::SetKeyCond(Down, []{
+			return CheckHitKey(KEY_INPUT_DOWN) != 0;
+		});
+	}
+	else{
+		// 反転
+		KeyInput::SetKeyCond(Left, []{
+			return CheckHitKey(KEY_INPUT_RIGHT) != 0;
+		});
+		KeyInput::SetKeyCond(Right, []{
+			return CheckHitKey(KEY_INPUT_LEFT) != 0;
+		});
+		KeyInput::SetKeyCond(Up, []{
+			return CheckHitKey(KEY_INPUT_DOWN) != 0;
+		});
+		KeyInput::SetKeyCond(Down, []{
+			return CheckHitKey(KEY_INPUT_UP) != 0;
+		});
+	}
 }
--- GameTemplate/GameTemplate/MainScene.h
+++ GameTemplate/GameTemplate/MainScene.h
@@ -3,6 +3,8 @@
 
 // ゲーム画面クラス
 class MainScene : public BaseScene{
+	bool reverseFlg;	// 反転フラグ
+	void SetKeys();	// キーの判定条件のセット
 public:
 	MainScene();	// コンストラクタ
 	~MainScene();	// デストラクタ
